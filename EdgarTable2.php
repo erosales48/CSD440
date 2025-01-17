2bfe2104186be2ecd2cbedbcf5bccc912cc6739a
erosales48 <erosales48@pm.me>
2025-01-16 19:43:15 -0500

    Enhance table functionality and modularize code.
    Added a function `sumNumbers` in a new external file (`EdgarTable3.php`) to compute cell values dynamically. Updated the table to include formatted sums and linked a new CSS stylesheet (`style.css`) for improved styling and structure.

--- EdgarTable2.php
+++ EdgarTable2.php
@@ -4,36 +4,23 @@
     January 15, 2021
 
     Write a program that creates an HTML table using a PHP nested loop structure.
-    In the loop structure you are to display a two dimensional table holding PHP generated random numbers in each cell.
+    In the loop structure you are to display a two-dimensional table holding PHP generated random numbers in each cell.
     Do not use PHP to display the actual table tags.
     This will require several opening and closing PHP tags.
 -->
+<?php require_once 'EdgarTable3.php'; ?>
 
 <html lang="en">
     <head>
         <title>Edgar Table2</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-            h1 {
-                text-align: center;
-            }
-
-            table {
-                width: 50%;
-                border-collapse: collapse;
-                margin: 20px auto;
-            }
-
-            th, td {
-                border: 1px solid black;
-                padding: 10px;
-                text-align: center;
-                border-collapse: collapse;
-        </style>
+        <link rel="stylesheet" href="style.css"
     </head>
-    <body>
+    <header>
         <h1>The Table of random numbers</h1>
+    </header>
+    <body>
         <hr />
         <table>
             <thead>
@@ -47,9 +34,20 @@
             <tbody>
                 <?php for ($i = 1; $i <= 5; $i++) { ?>
                     <tr>
-                        <td><?php echo "Row $i"; ?></td>
-                        <?php for ($j = 1; $j <= 5; $j++) { ?>
-                        <td><?php echo rand(1, 100); ?></td>
+                        <td style="font-weight: bold; color: red;" >
+                            <?php echo "Row $i"; ?>
+                        </td>
+                            <?php
+                                for ($j = 1; $j <= 5; $j++) {
+                                    $a=rand(1,100);
+                                    $b=rand(1,100);
+                            ?>
+                        <td style="color: royalblue">
+                            <?php
+                                echo "$a + $b = ";
+                                echo sumNumbers($a, $b);
+                            ?>
+                        </td>
                         <?php } ?>
                     </tr>
                 <?php } ?>
---
+++ EdgarTable3.php
@@ -0,0 +1,15 @@
+<!--
+    Edgar Rosales
+    CSD440 Server-Side Scripting
+    January 15, 2021
+
+    Starting with the PHP table created in Module 2, write a function that will be used 
+    to generate the value to be displayed in each cell. The function will take two random numbers 
+    as the parameters and will then return the sum. The function is to be placed in an external file.
+-->
+
+<?php
+    Function sumNumbers($num1, $num2) {
+                return $num1 + $num2;
+    }
+?>
